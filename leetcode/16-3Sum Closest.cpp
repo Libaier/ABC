44833fa219fd6d1f74178dde86b5ceeb68167862
Libaier <libaier.gc@gmail.com>
2016-03-10 21:27:32 +0800

    update leetcod16-3Sum Closest

---
+++ leetcode/16-3Sum Closest.cpp
@@ -0,0 +1,44 @@
+// Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+//     For example, given array S = {-1 2 1 -4}, and target = 1.
+
+//     The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int min = 99999999999999;
+        int result = nums[0]+nums[1]+nums[2];
+        // for(int i = 0;nums[i]<=target;i++)
+        for(int i = 0;i<nums.size();i++)
+        {
+            int begin = i+1;
+            int end = nums.size()-1;
+            while(begin<end)
+            {
+                int sum = nums[i]+nums[begin]+nums[end];
+                if(sum == target)
+                {
+                    return target;
+                }else if(sum > target)
+                {
+                    if(sum-target<min)
+                    {
+                        min = sum-target;
+                        result = sum;
+                    }
+                    end--;
+                }else{
+                    if(target-sum<min)
+                    {
+                        min = target-sum;
+                        result = sum;
+                    }
+                    begin++;
+                }
+            }
+        }
+        return result;
+
+    }
+};
