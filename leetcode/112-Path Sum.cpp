be6ece1de512cb7f2b1d515e5869b4c5cda41c9c
Libaier <libaier.gc@gmail.com>
2016-03-17 14:34:17 +0800

    update leetcode112. Path Sum

---
+++ leetcode/112-Path Sum.cpp
@@ -0,0 +1,61 @@
+//Given a binary tree and a sum, determine if the tree has a root - to - leaf path such that adding up all the values along the path equals the given sum.
+//
+//For example :
+//Given the below binary tree and sum = 22,
+//5
+/// \
+//4   8
+/// / \
+//11  13  4
+/// \      \
+//7    2      1
+//return true, as there exist a root - to - leaf path 5->4->11->2 which sum is 22.
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	bool hasPathSum(TreeNode* root, int sum) {
+		//vector<TreeNode*> temp;
+		//temp.push_back(root);
+
+		//vector<int> tempsum;
+		//tempsum.push_back(root->val);
+
+		//int tag = 0;
+
+		//while (!temp.empty())
+		//{
+		//	int n = temp.size();
+		//	for (; tag < n; i++)
+		//	{
+		//		if (temp[i]->left == NULL&&temp[i]->left == NULL&&tempsum->val == sum)
+		//		{
+		//			return false;
+		//		}
+		//	}
+		//}
+		if (root == NULL)
+		{
+			return false;
+		}
+		if (root->left == NULL&&root->right == NULL&&sum == root->val)
+		{
+			return true;
+		}
+
+		if (root->left == NULL&&root->right == NULL&&sum != root->val)
+		{
+			return false;
+		}
+
+		return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+
+	}
+};
