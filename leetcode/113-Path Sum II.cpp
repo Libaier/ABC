0e3eb5f20c8f58ca11996fed051b8377286a43aa
Libaier <libaier.gc@gmail.com>
2016-04-02 22:13:18 +0800

    update leetcode 113

--- leetcode/113-Path Sum II.cpp
+++ leetcode/113-Path Sum II.cpp
@@ -26,42 +26,68 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-    	vector<vector<int>> result;
-    	if (root==NULL)
-    	{
-    		/* code */
-    		return result;
-    	}
-        stack<TreeNode*> treeNodeTemp;
-        treeNodeTemp.push(root);
-        int sumTemp = 0;
-        while(!treeNodeTemp.empty())
-        {
-        	if(treeNodeTemp.top()->left==NULL&&treeNodeTemp.top()->right==NULL)
-        	{
-        		if(sum==sumTemp)
-        		{
-        			stack<TreeNode*> treeNodeTempTemp = treeNodeTemp;
-        			vector<int> resultTemp;
-        			while(!treeNodeTempTemp.empty())
-        			{
-						resultTemp.push_back(treeNodeTempTemp.top());
+	vector<vector<int>> pathSum(TreeNode* root, int sum) {
+		vector<vector<int>> result;
+		if (root == NULL)
+		{
+			/* code */
+			return result;
+		}
+		stack<TreeNode*> treeNodeTemp;
+		treeNodeTemp.push(root);
+		int sumTemp = root->val;
+		while (!treeNodeTemp.empty())
+		{
+			if (treeNodeTemp.top()->left != NULL&&treeNodeTemp.top()->left->val != INT_MAX)
+			{
+				sumTemp += treeNodeTemp.top()->left->val;
+				treeNodeTemp.push(treeNodeTemp.top()->left);
+				
+			}
+			else if (treeNodeTemp.top()->right != NULL&&treeNodeTemp.top()->right->val != INT_MAX){
+				sumTemp += treeNodeTemp.top()->right->val;
+				treeNodeTemp.push(treeNodeTemp.top()->right);
+			}
+
+			if (treeNodeTemp.top()->left == NULL&&treeNodeTemp.top()->right == NULL)
+			{
+				if (sum == sumTemp)
+				{
+					stack<TreeNode*> treeNodeTempTemp = treeNodeTemp;
+					vector<int> resultTemp;
+					while (!treeNodeTempTemp.empty())
+					{
+						resultTemp.push_back(treeNodeTempTemp.top()->val);
 						treeNodeTempTemp.pop();
-        			}
-        		}
-        		treeNodeTemp.top().val = INT_MAX;
-        		treeNodeTemp.pop();
-        	}
-        	if(treeNodeTemp.top()->left!=NULL&&treeNodeTemp.top()->left->val!=INT_MAX)
-        	{
-        		treeNodeTemp.push(treeNodeTemp.top()->left);
-        	}else if(treeNodeTemp.top()->right!=NULL&&treeNodeTemp.top()->right->val!=INT_MAX){
-        		treeNodeTemp.push(treeNodeTemp.top()->right);
-        	}
-        }
-        return result;
-    }
+					}
+					reverse(resultTemp.begin(),resultTemp.end());
+					result.push_back(resultTemp);
+				}
+				sumTemp = sumTemp - treeNodeTemp.top()->val;
+				treeNodeTemp.top()->val = INT_MAX;
+				treeNodeTemp.pop();
+			}else if((treeNodeTemp.top()->left == NULL&&treeNodeTemp.top()->right!=NULL&&treeNodeTemp.top()->right->val == INT_MAX)||
+			(treeNodeTemp.top()->right == NULL&&treeNodeTemp.top()->left != NULL&&treeNodeTemp.top()->left->val == INT_MAX)||
+			(treeNodeTemp.top()->right!=NULL&&treeNodeTemp.top()->right->val == INT_MAX
+			&&treeNodeTemp.top()->left != NULL&&treeNodeTemp.top()->left->val == INT_MAX))
+			{
+			    sumTemp = sumTemp - treeNodeTemp.top()->val;
+				treeNodeTemp.top()->val = INT_MAX;
+				treeNodeTemp.pop();
+			}
+
+		}
+		return result;
+	}
 };
