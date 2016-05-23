d976e44ae8f9ad55ed6a4ad19e6c92eb267506fb
Libaier <libaier.gc@gmail.com>
2016-05-23 16:22:59 +0800

    update hihocoder tire

---
+++ c++/const.md
@@ -0,0 +1,10 @@
+##const
+
+* 必须初始化
+* 不可修改
+
+##const指针
+
+* char* const p 指向地址不可修改
+
+* const char*  p/  char const*  p 所指内容不可修改
---
+++ code/leetcode/219-Contains Duplicate II.cpp
@@ -0,0 +1,9 @@
+// 219. Contains Duplicate II My Submissions QuestionEditorial Solution
+// Total Accepted: 53219 Total Submissions: 178921 Difficulty: Easy
+// Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+    }
+};
