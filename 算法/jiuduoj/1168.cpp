93e47392cc407e87a8ea84e7534667ee83921dbf
Libaier <libaier.gc@gmail.com>
2016-06-01 16:27:38 +0800

    update virtual method

---
+++ 算法/jiuduoj/1168.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s1;
+	char s[1000];
+	cin>>s1;//输入待删除短字符串
+
+	int j;
+	string s2;//用于保存待删除字符串
+	string::iterator i;//迭代器，其实就是数组下标
+	
+	cin.ignore();//一直PE，搞了半天才知道原来是输入流有残留，如果你也PE了，记得输入是写scanf("%d\n", &n);忽略掉一个回车，或者cin>>n;cin.ignore();忽略掉一个字符。这种情况往往发生在下一个输入是准备输入一整行时。
+
+	while(cin.getline(s,1000))
+	{
+		s2=s;
+		//循环删除s2中的s1
+		while(s2.find(s1)!=string::npos)//如果s2.find(s1)==string::npos则说明s2中不包含s1
+		{
+			i=s2.begin()+s2.find(s1);//找到s1位置
+			s2.erase(i,i+s1.length());//删除s1
+		}
+
+		//循环删除s2中的空格，与删除s1同理
+		while(s2.find(' ')!=string::npos)
+		{
+			i=s2.begin()+s2.find(' ');
+			s2.erase(i);
+		}
+		//输出删除后的字符串
+		cout<<s2<<endl;
+	}
+	return 0;
+}
--- 算法/算法模版/二叉树/Binary Tree Postorder Traversal.cpp
+++ 算法/算法模版/二叉树/Binary Tree Postorder Traversal.cpp
@@ -9,7 +9,104 @@
  */
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        
+	//preorder
+    void preorder(TreeNode* root,vector<int> &result) {
+        if (root==NULL) return;
+        result.push_back(root->val);
+        preorder(root->left,result);
+        preorder(root->right,result);
     }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        preorder(root,result);
+        return result;
+    }
+
+
+    //inorder
+    void inorder(TreeNode* root,vector<int> &result) {
+        if (root==NULL) return;
+        inorder(root->left,result);
+        result.push_back(root->val);
+        inorder(root->right,result);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root,result);
+        return result;   
+    }
+
+
+    //postorder
+    void postorder(TreeNode* root,vector<int> &result) {
+        if (root==NULL) return;
+        postorder(root->left,result);
+        postorder(root->right,result);
+        result.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        postorder(root,result);
+        return result;   
+    }
+
+    //levelOrder
+    void levelOrder(TreeNode* root,vector<vector<int>> &result,int depth) {
+        if (root==NULL) return;
+        if (result.size()==depth)
+        {
+        	result.push_back(vector<int>());
+        }
+        result[depth].push_back(root->val);
+        levelOrder(root->left,result,depth+1);
+        levelOrder(root->right,result,depth+1);
+    }
+
+    vector<vector<int>> levelOrderTraversal(TreeNode* root) {
+        vector<vector<int>> result;
+        levelOrder(root,result,0);
+        return result;  
+    }
+
+    // //levelOrder leetcode
+    // void levelOrderTraversal(TreeNode* root,vector<vector<int>> &result,int depth) {
+    //     if (root==NULL) return;
+    //     if (result.size()==depth)
+    //     {
+    //     	result.push_back(vector<int>());
+    //     }
+    //     result[depth].push_back(root->val);
+    //     levelOrderTraversal(root->left,result,depth+1);
+    //     levelOrderTraversal(root->right,result,depth+1);
+    // }
+
+    // vector<vector<int>> levelOrder(TreeNode* root) {
+    //     vector<vector<int>> result;
+    //     levelOrderTraversal(root,result,0);
+    //     return result;  
+    // }
+
+
+    //verticalOrder
+    
+    void verticalOrder(TreeNode* root,vector<vector<int>> &result,int depth) {
+        if (root==NULL) return;
+        if (result.size()==depth)
+        {
+        	result.push_back(vector<int>());
+        }
+        result[depth].push_back(root->val);
+        levelOrder(root->left,result,depth+1);
+        levelOrder(root->right,result,depth+1);
+    }
+
+    vector<vector<int>> verticalOrderTraversal(TreeNode* root) {
+        vector<vector<int>> result;
+        levelOrder(root,result,0);
+        return result;  
+    }
+
 };
