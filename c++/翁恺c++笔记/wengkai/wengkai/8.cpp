f637ccd783b746b9b8d311de4aa6d682aecb75b5
Libaier <libaier.gc@gmail.com>
2016-06-06 22:38:49 +0800

    update 翁凯1-10

---
+++ c++/翁恺c++笔记/wengkai/wengkai/8.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+class MyClass8
+{
+public:
+	MyClass8()
+	{
+		//a = 1;
+	}
+	void fun(){
+		printf("&(MyClass8.a):%p\n",&a);
+		printf("&(MyClass8.a):%p\n", this);
+	}
+	~MyClass8(){
+
+	}
+	int a;
+};
+
+void main()
+{
+
+	MyClass8 m;
+
+	printf("&m:%p\n", &m);
+	printf("&m.a:%p\n", &(m.a));
+	m.fun();
+
+	getchar();
+}
